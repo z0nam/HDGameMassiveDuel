6a4ae37a3ead4d485770b201762fe2a966038433
uno <unobure@gmail.com>
2015-12-11 03:36:21 +0900

    재귀함수를 빼야할 것 같은데...

--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 // 테스트용 데이터
 var student1 = {studentId: "1234-56789", name: "user1", maxMemory: 4,
+  lastStrategy: "",
   memories: {
     init: ['d', 1],
     dd: ['d', 1],
@@ -18,64 +19,64 @@ var student1 = {studentId: "1234-56789", name: "user1", maxMemory: 4,
     hddh: ['h', 1],
     hdhd: ['d', 1],
     hdhh: ['h', 1],
-    hhdd: ['d', 0.2],
+    hhdd: ['d', 1],
     hhdh: ['h', 1],
     hhhd: ['d', 1],
     hhhh: ['h', 1],
-    hhddhd: ['h', 0.3],
-    hhddhddh: ['h', 0.4]
+    hhddhd: ['h', 1],
+    hhddhddh: ['h', 1]
   },
   scores: Array(Array()),
   // history: ["hh", "dd", "hd", 'dh'],
   history: [],
   games: 0,
-  getStrategy: getStrategy,
+  getMemory: getMemory,
   makeDecision: makeDecision,
 };
 
 var student2 = {studentId: '12345-678', name: 'user2', maxMemory: 1,
+  lastStrategy: "",
   memories: {
-    init: ['h', 0.7],
-    dd: ['h', 0.3],
-    dh: ['d', 0.5],
-    hd: ['h', 0.7],
-    hh: ['d', 0.6]
+    init: ['h', 1],
+    dd: ['h', 1],
+    dh: ['d', 1],
+    hd: ['h', 1],
+    hh: ['d', 1]
   },
   // history: ["dh", "dd"],
   // scores: [[0, 0, 130, 105, 0, 130, 105, 0, 130, 0], [105, 0, 130]],
   history: [],
   scores: Array(Array()),
   games: 57,
-  getStrategy: getStrategy,
+  getMemory: getMemory,
   makeDecision: makeDecision,
 };
 
 // 전략을 가지고 d, h 중 무엇을 낼지 결정
 function makeDecision(round) {
-  var number = Math.random();
-  var strategy = this.getStrategy(round);
+  var randonNumber = Math.random();
+  var memoryNumber = Math.min(round - 1, this.maxMemory, this.history.length); // round마다 사용해야할 memory 숫자를 판단
+  var strategy = this.getMemory(memoryNumber, this.memories, this.history); // 실제 참고한 전략
+  lastStrategy = strategy;
   var char = strategy[0];
   var probability = strategy[1] || 1;
 
-  if (number <= probability && (char === 'd' || char === 'h')) {
-    return char;
-  } else {
+  if (randonNumber > probability) {
     switch (char) {
       case 'd':
-        return 'h';
+        char = 'h';
+        break;
       case 'h':
-        return 'd';
+        char = 'd';
+        break;
       default:
         console.log("error: strategy character");
     }
   }
+  return char;
 }
 
-// history와 비교하여 전략을 짬
-function getStrategy (round) {
-  var memoryNumber = Math.min(round - 1, this.maxMemory, this.history.length); // round마다 사용해야할 memory 숫자를 판단
-  return getMemory(memoryNumber, this.memories, this.history);
-
+  // history와 비교하여 전략을 짬
   function getMemory(num, memories, history) {
     var memory = [];
     var recentHistory = "";
@@ -93,9 +94,9 @@ function getStrategy (round) {
     } else {
       console.log("error: round value");
     }
+    console.log(num, recentHistory, memory);
     return memory;
   }
-}
 
 function unusableMemory(memory) {
   return !memory || ( memory[0] !== 'h' && memory[0] !== 'd'); // 못쓰는 값이면 true 반환
@@ -107,26 +108,8 @@ function duel(user1, user2, round) {
   user1.history.unshift(result); // 최근 결과가 앞에 옴.
   user2.history.unshift(result);
 
-  var score = [];
-  switch (result) {
-    case "dd":
-      score = [105, 105];
-      break;
-    case "dh":
-      score = [105, 130];
-      break;
-    case "hd":
-      score = [130, 105];
-      break;
-    case "hh":
-      score = [0, 0];
-      break;
-    default:
-      console.log("error: invalid result");
-  }
-
-  user1.scores[0][round - 1] = score[0]; // 몇번째 게임인지 알아내서 [0] 부분을 교체해야 함
-  user2.scores[0][round - 1] = score[1];
+  user1.scores[0][round - 1] = calScore(result)[0]; // 몇번째 게임인지 알아내서 [0] 부분을 교체해야 함
+  user2.scores[0][round - 1] = calScore(result)[1];
 
   putDuelLogs(user1, round);
   putDuelLogs(user2, round);
@@ -141,17 +124,13 @@ function duel(user1, user2, round) {
 function calScore(result) {
   switch (result) {
     case "dd":
-      score = [105, 105];
-      break;
+      return [105, 105];
     case "dh":
-      score = [105, 130];
-      break;
+      return [105, 130];
     case "hd":
-      score = [130, 105];
-      break;
+      return [130, 105];
     case "hh":
-      score = [0, 0];
-      break;
+      return [0, 0];
     default:
       console.log("error: invalid result");
   }
@@ -159,14 +138,19 @@ function calScore(result) {
 
 function putDuelLogs(user, round) {
   document.write("Round " + round + " " + user.name + ": ");
-  document.write(user.history[round-1] + " ");
-  document.write(user.scores[0][round-1] + "<br/>");
+  document.write(user.history[0] + " ");
+  document.write(user.scores[0][round-1] + " ");
+  document.write(user.lastStrategy + "<br/>");
 }
 
 function playGame(user1, user2) {
   for (var i = 1; i <= 10; i++) {
     duel(user1, user2, i);
   }
-    document.write("user1: " + user1.scores + " " + user1.history + "<br/>");
-    document.write("user2: " + user2.scores + " " + user2.history + "<br/>");
+    document.write("user1: " + user1.scores + " " + tempReverse(user1.history) + "<br/>");
+    document.write("user2: " + user2.scores + " " + tempReverse(user2.history) + "<br/>");
+}
+
+function tempReverse(array) {
+  return array.reverse();
 }
