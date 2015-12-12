fe392f1a78b0f182b623ae7ab699573fbc3ac48e
uno <unobure@gmail.com>
2015-12-13 03:43:40 +0900

    json 테스트 성공

--- data.php
+++ data.php
@@ -1,8 +1,11 @@
 <?php
-  $filename = "./data/test1.json";
-  $handle = fopen($filename, 'r') or die("File does not exist.");
-  $text = fread($handle, filesize($filename));
-  fclose($handle);
+  $filename = "./data/test.json";
+  // $handle = fopen($filename, 'r') or die("File does not exist.");
+  // $text = fread($handle, filesize($filename));
+  // fclose($handle);
+  $text = file_get_contents($filename);
+  // echo $text;
+  $json = json_decode($text);
+  // echo json_encode($json);
   echo $text;
-  // echo json_encode($text);
 ?>
